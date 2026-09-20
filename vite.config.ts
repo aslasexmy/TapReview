a7b92f95fdda3bb03139f8441c150f8385b7788f
aslasexmy <aslas.ex.my@gmail.com>
2026-09-20 15:05:51 +0800

    Add files via upload
    Update to TaptoGo

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,38 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { siteContent } from "./src/siteContent.js";
+const escapeHtml = (s: string) =>
+  s
+    .replaceAll("&", "&amp;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;");
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "site-content-seo",
+      transformIndexHtml: {
+        order: "pre",
+        handler: (html) =>
+          html
+            .replaceAll("%SITE_TITLE%", escapeHtml(siteContent.seo.title))
+            .replaceAll(
+              "%SITE_DESCRIPTION%",
+              escapeHtml(siteContent.seo.description),
+            ),
+      },
+    },
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
